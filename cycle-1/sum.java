fa2d2a51a1f1e031d9f71133081ee8019e63d605
ADITHYA-RITHAMBARA <98386259+ADITHYA-RITHAMBARA@users.noreply.github.com>
2022-01-25 21:10:00 +0530

    Update sum.java

--- cycle-1/sum.java
+++ cycle-1/sum.java
@@ -1,1 +1,14 @@
 
+import java.util.*;
+class sum
+{
+    public static void main(String args[])
+{
+    System.out.println("Enter two numbers ");
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    int c=a+b;
+    System.out.println("Sum is "+c);
+}
+}
